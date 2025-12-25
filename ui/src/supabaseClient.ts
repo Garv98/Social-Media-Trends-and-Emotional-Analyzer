98ba0171f2821b485d37d97f26104469a7c26498
Aditya-Ranjan1234 <aditya87331@gmail.com>
2025-12-25 23:01:58 +0530

    hello

--- ui/src/supabaseClient.ts
+++ ui/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fqhoqakgrbctbnygfujr.supabase.co';
-const supabaseKey = 'sb_publishable_ZcXrTyxcSnzJPpW8sVNwBw_ZlMrmGeD';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
